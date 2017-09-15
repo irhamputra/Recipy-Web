7a57d9d6aed8ef30c37d030b5fa0067e01c924f6
irhamputra <irhamputraprasetyo@gmail.com>
2017-09-15 15:47:41 +0200

    creating model and frontend for recipe

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A new Recipe", "Test Recipe", "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg")
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
